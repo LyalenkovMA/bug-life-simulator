be3688289b99b5dd809089afd70eed200ea17f74
LyalenkovMA <167408417+LyalenkovMA@users.noreply.github.com>
2026-01-06 15:35:21 +0300

    подготовка проекта 0000002

---
+++ bug-life-simulator/src/Core/.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bug-life-simulator.Content.Audio
+{
+    internal class _
+{
+}
+}
